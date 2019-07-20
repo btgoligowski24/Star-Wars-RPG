1714ea14c5bde4c48d5099227add12d6e7bfb901
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-07-20 15:25:08 -0500

    refactoring code to remove as many global vars as possible

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,5 @@
 $(document).ready(function () {
 
-    var cardDeckDiv = $("<div class=\"card-deck\">");
-    var availCharsElem = $("#availChars");
-    var yourCharElem = $("#yourChar");
-    var charName = "";
-    var yourStats = [];
-    var enemyStats = [];
-    var enemiesElem = $("#enemies");
-    var fightElem = $("#fight");
-    var attackElem = $("#attack");
-    var myAttackElem = $("#myAttack");
-    var thierAttackElem = $("#theirAttack");
-    var newAttackPower = 0;
     var chars = [{
         name: "Rey",
         attack: 6,
@@ -38,39 +26,14 @@ $(document).ready(function () {
         img: "assets/images/darthVader.jfif"
     }];
 
-    function generateChars() {
-        $(availCharsElem).append(cardDeckDiv);
-        $.each(chars, function (index, key) {
-            var cardDiv = $("<div>");
-            var cardBodyDiv = $("<div>");
-            var cardHeader = $("<h5>");
-            var cardImg = $("<img>");
-            var cardFooter = $("<p>");
-            if (index === 0) {
-                $(cardDiv).attr("class", "card text-center mr-1 my-2 bgNormal");
-            } else {
-                $(cardDiv).attr("class", "card text-center mx-1 my-2 bgNormal");
-            }
-            $(cardDeckDiv).append(cardDiv);
-            $(cardBodyDiv).attr("class", "card-body py-1 px-2");
-            $(cardDiv).append(cardBodyDiv);
-            $(cardHeader).attr("class", "card-title playerName mb-2");
-            $(cardHeader).text(key.name);
-            $(cardBodyDiv).append(cardHeader);
-            $(cardImg).attr({
-                src: key.img,
-                class: "card-img-top playerImg rounded",
-                alt: key.name + " image was supposed to go here."
-            });
-            $(cardBodyDiv).append(cardImg);
-            $(cardFooter).attr("class", "card-title playerHP mt-2 mb-0");
-            $(cardFooter).text("hp: " + key.hp);
-            $(cardBodyDiv).append(cardFooter);
-        })
-    }
     generateChars();
 
-    var charElems = $.makeArray($("#availChars .card"));
+    function generateChars() {
+        var cardDeckDiv = $("<div class=\"card-deck\">");
+        var availCharsElem = $("#availChars");
+        $(availCharsElem).append(cardDeckDiv);
+        $.each(chars, makeCards)
+    }
 
     function removeChar(value) {
         $(value).detach();
@@ -96,58 +59,101 @@ $(document).ready(function () {
     }
 
     function getStats(val, stats) {
-        charName = $(val).children().children(".playerName").text();
+        var charName = $(val).children().children(".playerName").text();
         stats = $.grep(chars, function (match) {
             return match.name === charName
         })
         return stats
     }
 
-    $(charElems).each(function (index, value) {
-        $(value).on("click", function () {
-            var newCardDeckDiv = $("<div class=\"card-deck\">");
-            if ($("#availChars .card-deck").length > 0) {
-                $(availCharsElem).css("display", "none");
-                yourStats = getStats(value);
-                removeChar(value);
-                $(yourCharElem).append(newCardDeckDiv);
-                $(newCardDeckDiv).append(value);
-                $(cardDeckDiv).detach();
-                $(enemiesElem).append(cardDeckDiv);
-                $("#enemies .card-deck .card").each(function (index, value) {
-                    changeBGColor(value);
-                    resetCardDeckMargin(index, value);
-                })
-            }
-            if ($(value).parent().parent().attr("id") !== "yourChar" && $(value).parent().parent().attr("id") !== "fight") {
-                if ($("#enemies .card").length > 0 && $("#fight .card").length === 0) {
-                    removeChar(value);
-                    enemyStats = getStats(value);
-                    $(fightElem).append(newCardDeckDiv);
-                    $(newCardDeckDiv).append(value);
-                    if ($("#enemies .card-deck .card").eq(0).hasClass("mx-1")) {
-                        $("#enemies .card-deck .card").eq(0).removeClass("mx-1").addClass("mr-1");
-                    }
-                    resetCardMargin(value);
-                    changeBGColor(value);
-                } else {
-                    alert("You already have an enemy to fight!");
+    function makeCards(index, key) {
+        var findCardDeckElem = $(".card-deck")
+        var cardDiv = $("<div>");
+        var cardBodyDiv = $("<div>");
+        var cardHeader = $("<h5>");
+        var cardImg = $("<img>");
+        var cardFooter = $("<p>");
+        if (index === 0) {
+            $(cardDiv).attr("class", "card text-center mr-1 my-2 bgNormal");
+        } else {
+            $(cardDiv).attr("class", "card text-center mx-1 my-2 bgNormal");
+        }
+        $(findCardDeckElem).append(cardDiv);
+        $(cardBodyDiv).attr("class", "card-body py-1 px-2");
+        $(cardDiv).append(cardBodyDiv);
+        $(cardHeader).attr("class", "card-title playerName mb-2");
+        $(cardHeader).text(key.name);
+        $(cardBodyDiv).append(cardHeader);
+        $(cardImg).attr({
+            src: key.img,
+            class: "card-img-top playerImg rounded",
+            alt: key.name + " image was supposed to go here."
+        });
+        $(cardBodyDiv).append(cardImg);
+        $(cardFooter).attr("class", "card-title playerHP mt-2 mb-0");
+        $(cardFooter).text("hp: " + key.hp);
+        $(cardBodyDiv).append(cardFooter);
+    }
+
+    function charInteraction(value) {
+        var oldCardDeckDiv = $("#availChars .card-deck");
+        var newCardDeckDiv = $("<div class=\"card-deck\">");
+        var hideAvailCharsElem = $("#availChars");
+        var yourCharElem = $("#yourChar");
+        var yourStats = [];
+        var enemyStats = [];
+        var enemiesElem = $("#enemies");
+        var fightElem = $("#fight");
+        if ($("#availChars .card-deck").length > 0) {
+            $(hideAvailCharsElem).css("display", "none");
+            yourStats = getStats(this);
+            removeChar(this);
+            $(yourCharElem).append(newCardDeckDiv);
+            $(newCardDeckDiv).append(this);
+            $(oldCardDeckDiv).detach();
+            $(enemiesElem).append(oldCardDeckDiv);
+            $("#enemies .card-deck .card").each(function (index, value) {
+                changeBGColor(value);
+                resetCardDeckMargin(index, value);
+            })
+        }
+        if ($(this).parent().parent().attr("id") !== "yourChar" && $(this).parent().parent().attr("id") !== "fight") {
+            if ($("#enemies .card").length > 0 && $("#fight .card").length === 0) {
+                removeChar(this);
+                enemyStats = getStats(this);
+                $(fightElem).append(newCardDeckDiv);
+                $(newCardDeckDiv).append(this);
+                if ($("#enemies .card-deck .card").eq(0).hasClass("mx-1")) {
+                    $("#enemies .card-deck .card").eq(0).removeClass("mx-1").addClass("mr-1");
                 }
+                resetCardMargin(this);
+                changeBGColor(this);
+            } else {
+                alert("You already have an enemy to fight!");
             }
-        })
-    })
-    
-    $(attackElem).on("click", function () {
+        }
+    }
+
+    function attacking() {
+        var myAttackElem = $("#myAttack");
+        var thierAttackElem = $("#theirAttack");
+        var newAttackPower = 0;
         if (!$("#yourChar .card").length) {
             alert("You must choose a character first!");
         } else {
             if (!$("#fight .card").length) {
-                $("myAttack").text("No enemy to attack");
-                $("theirAttack").text("");
+                $("#myAttack").text("No enemy to attack");
+                $("#theirAttack").text("");
             } else {
 
             }
         }
+    }
+
+    $($.makeArray($("#availChars .card"))).each(function (index, value) {
+        $(value).on("click", charInteraction)
     })
+
+    $("#attack").on("click", attacking)
 
 })
