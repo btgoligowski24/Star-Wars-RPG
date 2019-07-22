50f8edd8d90879e553291560ecda953b3cf64148
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-07-22 00:47:47 -0500

    i think i have this game working decently for mobile now too

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,37 +32,29 @@ $(document).ready(function () {
     generateChars();
 
     function generateChars() {
-        var cardDeckDiv = $("<div class=\"card-deck\">");
+        var row1Elem = $("<div>");
+        var col1Elem = $("<div>");
+        var row2Elem = $("<div>")
         var availCharsElem = $("#availChars");
-        $(availCharsElem).append(cardDeckDiv);
+        $(row1Elem).attr("class", "row row1");
+        $(col1Elem).attr("class", "col-xl-7 column1");
+        $(row2Elem).attr("class", "row mx-0 row2");
+        $(availCharsElem).append(row1Elem);
+        $(row1Elem).append(col1Elem);
+        $(col1Elem).append(row2Elem);
         $.each(chars, makeCards)
     }
 
     function removeChar(value) {
         $(value).detach();
-        if ($(value).hasClass("mx-1")) {
-            $(value).removeClass("mx-1");
-        }
-    }
-
-    function resetCardMargin(value) {
-        if ($(value).hasClass("mx-1")) {
-            $(value).removeClass("mx-1").addClass("mr-1");
-        }
-    }
-
-    function resetCardDeckMargin(index, value) {
-        if (index === 0) {
-            resetCardMargin(value);
-        }
     }
 
     function changeBGColor(value) {
-        $(value).removeClass("bgNormal").addClass($(value).parent().parent().attr("data-bgColor"));
+        $(value).children().removeClass("bgNormal").addClass($(value).parents().eq(3).attr("data-bgColor"));
     }
 
     function getStats(val, stats) {
-        var charName = $(val).children().children(".playerName").text();
+        var charName = $(val).children().children().children(".playerName").text();
         stats = $.grep(chars, function (match) {
             return match.name === charName
         })
@@ -86,18 +78,17 @@ $(document).ready(function () {
     }
 
     function makeCards(index, key) {
-        var findCardDeckElem = $(".card-deck")
+        var findRow2Elem = $(".row2")
+        var col2Elem = $("<div>");
         var cardDiv = $("<div>");
         var cardBodyDiv = $("<div>");
         var cardHeader = $("<h5>");
         var cardImg = $("<img>");
         var cardFooter = $("<p>");
-        if (index === 0) {
-            $(cardDiv).attr("class", "card text-center mr-1 my-2 bgNormal");
-        } else {
-            $(cardDiv).attr("class", "card text-center mx-1 my-2 bgNormal");
-        }
-        $(findCardDeckElem).append(cardDiv);
+        $(col2Elem).attr("class", "col-xl-3 col-md-4 col-sm-6 p-sm-1 p-md-0 mt-2 column2");
+        $(cardDiv).attr("class", "card text-center m-1 mx-md-1 my-md-0 bgNormal");
+        $(findRow2Elem).append(col2Elem);
+        $(col2Elem).append(cardDiv);
         $(cardBodyDiv).attr("class", "card-body py-1 px-2");
         $(cardDiv).append(cardBodyDiv);
         $(cardHeader).attr("class", "card-title playerName mb-2");
@@ -115,40 +106,44 @@ $(document).ready(function () {
     }
 
     function charInteraction(value) {
-        var oldCardDeckDiv = $("#availChars .card-deck");
-        var newCardDeckDiv = $("<div class=\"card-deck\">");
+        var oldRow1Elem = $("#availChars .row1");
+        var row1Elem = $("<div>");
+        var col1Elem = $("<div>");
+        var row2Elem = $("<div>");
         var hideAvailCharsElem = $("#availChars");
         var yourCharElem = $("#yourChar");
         var enemiesElem = $("#enemies");
         var fightElem = $("#fight");
-        if ($("#availChars .card-deck").length > 0) {
+        $(row1Elem).attr("class", "row row1");
+        $(col1Elem).attr("class", "col-xl-7 column1");
+        $(row2Elem).attr("class", "row mx-0 row2");
+        if ($("#availChars .row1").length > 0) {
             $(hideAvailCharsElem).css("display", "none");
             yourStats = getStats(this);
             removeChar(this);
-            $(yourCharElem).append(newCardDeckDiv);
-            $(newCardDeckDiv).append(this);
-            $(oldCardDeckDiv).detach();
-            $(enemiesElem).append(oldCardDeckDiv);
+            $(yourCharElem).append(row1Elem);
+            $(row1Elem).append(col1Elem);
+            $(col1Elem).append(row2Elem);
+            $(row2Elem).append(this);
+            $(oldRow1Elem).detach();
+            $(enemiesElem).append(oldRow1Elem);
             toggleChooseEnemy();
-            $("#enemies .card-deck .card").each(function (index, value) {
+            $("#enemies .column2").each(function (index, value) {
                 changeBGColor(value);
-                resetCardDeckMargin(index, value);
             })
         }
-        if ($(this).parent().parent().attr("id") !== "yourChar" && $(this).parent().parent().attr("id") !== "fight") {
-            if ($("#enemies .card").length > 0 && $("#fight .card").length === 0) {
+        if ($(this).parents().eq(3).attr("id") !== "yourChar" && $(this).parents().eq(3).attr("id") !== "fight") {
+            if ($("#enemies .column2").length > 0 && $("#fight .column2").length === 0) {
                 removeChar(this);
                 enemyStats = getStats(this);
-                $(fightElem).append(newCardDeckDiv);
-                $(newCardDeckDiv).append(this);
+                $(fightElem).append(row1Elem);
+                $(row1Elem).append(col1Elem);
+                $(col1Elem).append(row2Elem);
+                $(row2Elem).append(this);
                 toggleChooseEnemy();
                 if ($("#myAttack").text()) {
                     $("#myAttack").text("");
                 }
-                if ($("#enemies .card-deck .card").eq(0).hasClass("mx-1")) {
-                    $("#enemies .card-deck .card").eq(0).removeClass("mx-1").addClass("mr-1");
-                }
-                resetCardMargin(this);
                 changeBGColor(this);
             } else {
                 alert("You already have an enemy to fight!");
@@ -159,10 +154,10 @@ $(document).ready(function () {
     function attacking() {
         var myAttackElem = $("#myAttack");
         var thierAttackElem = $("#theirAttack");
-        if (!$("#yourChar .card").length) {
+        if (!$("#yourChar .column2").length) {
             alert("You must choose a character first!");
         } else {
-            if (!$("#fight .card").length) {
+            if (!$("#fight .column2").length) {
                 $("#myAttack").text("No enemy to attack");
                 $("#theirAttack").text("");
             } else {
@@ -172,8 +167,8 @@ $(document).ready(function () {
                     newAttackPower = newAttackPower + yourStats.attack;
                     enemyStats.hp = enemyStats.hp - newAttackPower;
                     if (enemyStats.hp <= 0) {
-                        $("#fight .card").detach();
-                        if (!$("#fight .card").length && !$("#enemies .card").length) {
+                        $("#fight .column2").detach();
+                        if (!$("#fight .column2").length && !$("#enemies .column2").length) {
                             $(myAttackElem).text("Game over, you win!!! You are a TRUE JEDI MASTER! Click restart to play as someone else.");
                             $(thierAttackElem).text("");
                             $("#restart").text("Restart");
@@ -200,7 +195,7 @@ $(document).ready(function () {
         }
     }
 
-    $($.makeArray($("#availChars .card"))).each(function (index, value) {
+    $($.makeArray($("#availChars .column2"))).each(function (index, value) {
         $(value).on("click", charInteraction)
     });
 
